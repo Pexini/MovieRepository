3f312e339ea0d2f6eb0f8bebfdd669eb3cd7e1d2
Pavel Davydov <Davydov.works@gmail.com>
2023-08-09 02:37:36 -0400

    add Mockito +@testWithjMoskito

--- src/test/java/ru_netilogy/MoviesRepositoryTest.java
+++ src/test/java/ru_netilogy/MoviesRepositoryTest.java
@@ -1,15 +1,24 @@
 package ru_netilogy;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MoviesRepositoryTest {
+public class MoviesRepositoryTest {
+    MoviesRepository repo =  Mockito.mock (MoviesRepository.class);
+
+
     MoviesItem movies1 = new MoviesItem(" Бладшот ", " Боевик ", LocalDate.of(2023, 12, 28));
-    MoviesItem movies2 = new MoviesItem(" Вперед ", " Мультфильм ",  LocalDate.of(2023, 7, 4));
+    MoviesItem movies2 = new MoviesItem(" Вперед ", " Мультфильм ", LocalDate.of(2023, 7, 4));
     MoviesItem movies3 = new MoviesItem(" Отель Белград ", " Комедия ", LocalDate.of(2023, 2, 24));
     MoviesItem movies4 = new MoviesItem(" Джентельмены ", " Боевик ", LocalDate.of(2023, 8, 12));
     MoviesItem movies5 = new MoviesItem(" Человек невидимка ", " Ужасы ", LocalDate.of(2023, 10, 8));
@@ -159,5 +168,18 @@ class MoviesRepositoryTest {
             System.out.println("Дата выхода: " + movie.getReleaseDate());
             System.out.println();
         }
+
+    }
+
+    @Test
+    public void getAllWithMockito() {
+        MoviesItem[] movies = {movies7, movies6, movies5, movies4, movies3, movies2, movies1};
+        doReturn(movies).when(repo).findAll();
+
+        MoviesItem[] expected = {movies7, movies6, movies5, movies4, movies3, movies2, movies1};
+        MoviesItem[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
+
